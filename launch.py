e8a71f1a6e4e18ec77e4f7a7f12ec26c619ad867
Chris Le Sueur <c.m.lesueur@gmail.com>
2014-04-01 01:34:11 +0100

    Oh yeah, fix tabs for launch.py too

--- launch.py
+++ launch.py
@@ -5,6 +5,7 @@ import pygame.locals
 import sys
 import os
 import cProfile
+import pstats
 
 sys.path.append(os.path.join('.', 'src'))
 
@@ -18,18 +19,20 @@ from quest import MainQuest
 PROFILE = False
 
 if __name__=='__main__':
-	pygame.init()
-	screen = pygame.display.set_mode((512, 512))
-	screen.fill((255, 255, 255))
+    pygame.init()
+    screen = pygame.display.set_mode((512, 512))
+    screen.fill((255, 255, 255))
 
-	#room = Room('start', 'A boring starting room')
-	player = Player('you', 'The Player')
-	world = world.World(player)
-	#world.add_objective(MainQuest(world))
-	if PROFILE:
-		cProfile.run('world.main_loop(screen)')
-	else:
-		world.main_loop(screen)
+    #room = Room('start', 'A boring starting room')
+    player = Player('you', 'The Player')
+    world = world.World(player)
+    #world.add_objective(MainQuest(world))
+    if PROFILE:
+        cProfile.run('world.main_loop(screen)', 'profiledump')
+        p = pstats.Stats('profiledump')
+        p.sort_stats('cumtime').print_stats(.5)
+    else:
+        world.main_loop(screen)
 
     #table = load_tile_table("/home/fish/Pictures/M_BISON_YESSSSSSS.jpg", 24, 16)
     #for x, row in enumerate(table):
